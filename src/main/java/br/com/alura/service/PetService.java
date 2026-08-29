da5e25bf1e0b7905e562ac47ecc37ff2df7922f3
Jean Matheus <jeanmatheus0612@gmail.com>
2026-08-28 23:32:14 -0300

    remove imports

--- src/main/java/br/com/alura/service/PetService.java
+++ src/main/java/br/com/alura/service/PetService.java
@@ -3,16 +3,11 @@ package br.com.alura.service;
 import br.com.alura.client.ClientHttpConfiguration;
 import br.com.alura.domain.Pet;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
